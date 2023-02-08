5d40adabf7e84f6efee39df241ab715713e30ab9
roysreejan <sreejanroy01@gmail.com>
2023-02-08 08:31:29 +0600

    Get the process name and the PID

--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "watchdog.hpp"
-
-int main(int argc, char* argv[]) {
-    Watchdog watchdog("config/config.json", std::chrono::seconds(10)); // Create a watchdog object
-    watchdog.start(); // Start the watchdog
-    return 0;
-}
--- src/watchdog.cpp
+++ src/watchdog.cpp
@@ -1,55 +1,51 @@
-#include "watchdog.hpp"
-#include <fstream>
 #include <iostream>
-#include <thread>
+#include <string>
+#include <vector>
+#include <fstream>
 
-Watchdog::Watchdog(const std::string& config_file_path, std::chrono::milliseconds config_reload_interval) 
-    : config_file_path(config_file_path), config_reload_interval(config_reload_interval) { 
-    reload_config(); // Load the configuration file
-}
+using namespace std;
 
-void Watchdog::reload_config() {
-    std::ifstream config_file(config_file_path); // Open the configuration file
-    config_file >> config; // Read the configuration file into the config variable
-}
-
-void Watchdog::start() { // Start the watchdog
-    while (true) { // Loop forever
-        // Do the monitoring logic here
-        // Check the status of each process listed in the configuration
-        for (const auto& process_config : config["processes"]) { // Loop through each process
-            std::string process_name = process_config["name"]; // Get the name of the process
-            std::string process_executable = process_config["executable"]; // Get the executable of the process
-            std::vector<std::string> process_arguments = process_config["arguments"]; // Get the arguments of the process
-
-            // Use a system call to check if the process is running
-            std::string cmd = "ps -C " + process_executable + " > /dev/null"; // Create the command
-            int result = system(cmd.c_str()); // Execute the command
-            if (result != 0) { // Check if the process is not running
-                // The process is not running, restart it
-                std::string process_start_command = process_executable; // Create the command to start the process
-                for (const auto& argument : process_arguments) { // Add the arguments to the command
-                    process_start_command += " " + argument; // Add the argument to the command
-                }
-                int start_result = system(process_start_command.c_str()); // Execute the command
-                if (start_result == 0) { // Check if the process was started successfully
-                    std::cout << process_name << " has been restarted." << std::endl; // Log that the process has been restarted
-                } else {
-                    std::cerr << "Error restarting " << process_name << "." << std::endl; // Log that there was an error restarting the process
-                }
-            }
-        }
-        // Check for changes in the configuration file
-        std::ifstream config_file(config_file_path); // Open the configuration file
-        nlohmann::json new_config; // Create a new variable to store the new configuration
-        config_file >> new_config; // Read the configuration file into the new variable
-        if (new_config != config) { // Check if the configuration has changed
-            config = new_config; // Update the configuration
-            // Log the change in the configuration
-            std::cout << "Configuration reloaded." << std::endl; // Log the change in the configuration
-        }
-
-        // Sleep for the specified interval before checking the configuration again
-        std::this_thread::sleep_for(config_reload_interval); // Sleep for the specified interval
+int main(){
+    // Read the config file: config.txt
+    ifstream config_file("config.txt");
+    string line;
+    vector<string> config;
+    while(getline(config_file, line)){
+        config.push_back(line);
     }
+
+    // foreach over config vector
+    for (auto &i : config){
+        // TODO: further processing of the line 
+        // typecast i to string
+        string process_name = i;
+
+        // issue pidof -s <process_name> command and read the output into a string
+        string command = "pidof -s " + process_name;
+        FILE *in;
+        char buff[512];
+        if(!(in = popen(command.c_str(), "r"))){
+            return 1;
+        }
+        string result = "";
+        while(fgets(buff, sizeof(buff), in)!=NULL){
+            result += buff;
+        }
+        pclose(in);
+
+        // if the output is empty, then the process is not running
+        if (result == ""){
+            cout << "Process " << process_name << " is not running" << endl;
+        }
+
+        // if the output is not empty, then the process is running
+        else{
+            cout << "Process " << process_name << " is running" << endl;
+            // print the pid of the process
+            cout << "PID: " << result << endl;
+        }
+
+    }
+
+    
 }
