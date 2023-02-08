3c6813c63ca34e86ac8775e669245df31ba69912
roysreejan <sreejanroy01@gmail.com>
2023-02-08 10:18:16 +0600

    logged the process name and the PID

--- src/watchdog.cpp
+++ src/watchdog.cpp
@@ -5,47 +5,117 @@
 
 using namespace std;
 
-int main(){
-    // Read the config file: config.txt
-    ifstream config_file("config.txt");
+void log(string message)
+{
+    // if the log file does not exist, create it
+    // if the log file exists, append the message to it
+    ofstream log_file;
+    log_file.open("log.txt", ios::app);
+    log_file << message << endl;
+    log_file.close();
+}
+
+bool isProcessRunning(string process_name)
+{
+    // issue pidof -s <process_name> command and read the output into a string
+    string command = "pidof -s " + process_name;
+    FILE *in;
+    char buff[512];
+    if (!(in = popen(command.c_str(), "r")))
+    {
+        return 1;
+    }
+    string result = "";
+    while (fgets(buff, sizeof(buff), in) != NULL)
+    {
+        result += buff;
+    }
+    pclose(in);
+
+    // if the output is empty, then the process is not running
+    if (result == "")
+    {
+        return false;
+    }
+
+    // if the output is not empty, then the process is running
+    else
+    {
+        return true;
+    }
+}
+
+
+bool lauchProcess(string process_name)
+{
+    // launch the process using the command: <process_name> & 
+    string command = process_name + " &";
+    system(command.c_str());
+
+    // get the pid of the process
+    command = "pidof -s " + process_name;
+    FILE *in;
+    char buff[512];
+    if (!(in = popen(command.c_str(), "r")))
+    {
+        return 1;
+    }
+    string result = "";
+    while (fgets(buff, sizeof(buff), in) != NULL)
+    {
+        result += buff;
+    }
+    pclose(in);
+
+    // log the pid of the process
+    log("Launched " + process_name + " with PID " + result);
+
+    return true;
+}
+
+int main()
+{
+    // list of processes to watch from config.txt file
+    vector<string> processes;
+    ifstream config_file;
+    config_file.open("config.txt");
     string line;
-    vector<string> config;
-    while(getline(config_file, line)){
-        config.push_back(line);
+    while (getline(config_file, line))
+    {
+        processes.push_back(line);
     }
+    config_file.close();
 
-    // foreach over config vector
-    for (auto &i : config){
-        // TODO: further processing of the line 
-        // typecast i to string
-        string process_name = i;
-
-        // issue pidof -s <process_name> command and read the output into a string
-        string command = "pidof -s " + process_name;
-        FILE *in;
-        char buff[512];
-        if(!(in = popen(command.c_str(), "r"))){
-            return 1;
+    // loop forever
+    while (true)
+    {
+        // check if each process is running
+        for (int i = 0; i < processes.size(); i++)
+        {
+            // if the process is not running, launch it
+            if (!isProcessRunning(processes[i]))
+            {
+                lauchProcess(processes[i]);
+            }
+            // if the process is running log it with PID
+            else
+            {
+                string command = "pidof -s " + processes[i];
+                FILE *in;
+                char buff[512];
+                if (!(in = popen(command.c_str(), "r")))
+                {
+                    return 1;
+                }
+                string result = "";
+                while (fgets(buff, sizeof(buff), in) != NULL)
+                {
+                    result += buff;
+                }
+                pclose(in);
+                log(processes[i] + " is running with PID " + result);
+            }
         }
-        string result = "";
-        while(fgets(buff, sizeof(buff), in)!=NULL){
-            result += buff;
-        }
-        pclose(in);
-
-        // if the output is empty, then the process is not running
-        if (result == ""){
-            cout << "Process " << process_name << " is not running" << endl;
-        }
-
-        // if the output is not empty, then the process is running
-        else{
-            cout << "Process " << process_name << " is running" << endl;
-            // print the pid of the process
-            cout << "PID: " << result << endl;
-        }
-
+        return 0;
     }
-
-    
 }
